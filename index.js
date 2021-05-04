9bc500c9bbef65463a354d0d83661bd0451a5e28
Emmanuel <emmanuelamon92@gmail.com>
2021-05-04 16:06:42 -0400

    commit tree 302541ec1e433aac5f3bbca8ea30f626fa7f2495

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    const firstDrivers = function(){
+        return drivers.slice(0,2)
+    }
+    return firstDrivers()
+}
+
+function returnLastTwoDrivers(drivers){
+    const lastDrivers = function(){
+      return drivers.slice(2)
+    }
+    return lastDrivers();
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// function createFareMultiplier(int){
+//     return function(){
+//         int * int;
+//     };
+// }
+
+function createFareMultiplier(outerInteger){
+    return function(fare){
+    // function multFare(){
+       return fare * outerInteger;
+    };
+    
+    
+}
+
+function fareDoubler(fare){
+    
+    const doubleFare = createFareMultiplier(2);
+    // console.log('createFareMultiplier ' + createFareMultiplier(fare)(2) )
+    //function doubleFare gives the parameter 'fare' to the function 
+    //inside of createFareMultiplier and '2' is the param for the outer function
+    return doubleFare(fare)
+}
+
+function fareTripler(fare){
+    console.log(fare)
+    const tripleFare = createFareMultiplier(3);
+    return tripleFare(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driversFunc){
+    return driversFunc(arrayOfDrivers);
+
+}
